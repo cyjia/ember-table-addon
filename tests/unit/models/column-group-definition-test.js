a6d6c01d912c1c9f1b8369b730d9f57fbe037bff
Chaoyang <cyjia@thoughtworks.com>
2015-05-27 15:44:40 +0800

    stop resizing column group when reached min width of last inner column.

--- tests/unit/models/column-group-definition-test.js
+++ tests/unit/models/column-group-definition-test.js
@@ -80,3 +80,50 @@ test('should resize last inner column when group size become smaller', function(
 
   assert.ok(thirdColumn.get('savedWidth') === widthBefore + widthAdd);
 });
+
+
+module('column group definition min width', {
+  beforeEach: function() {
+    firstColumn = ColumnDefinition.create({
+      headerCellName: 'Column1',
+      minWidth: 25,
+      getCellContent: function(row) {
+        return row.get('c');
+      }
+    });
+    secondColumn = ColumnDefinition.create({
+      headerCellName: 'Column2',
+      minWidth: 25,
+      getCellContent: function(row) {
+        return row.get('b');
+      }
+    });
+    group = ColumnGroupDefinition.create({
+      headerCellName: 'Group1',
+      innerColumns: [firstColumn, secondColumn]
+    });
+  }
+});
+
+test('min width', function(assert) {
+  console.log(group.get('minWidth'));
+  assert.ok(group.get('minWidth') === 175,
+      'should be sum of width of first column and min width of last column ');
+});
+
+test('depends on innerColumns.@each.width', function(assert) {
+  group.get('minWidth');
+
+  firstColumn.resize(100);
+
+  assert.ok(group.get('minWidth') === 125, 'should recompute when inner column changes width');
+});
+
+
+test('depends on innerColumns.lastObject.minWidth', function(assert) {
+  group.get('minWidth');
+
+  secondColumn.set('minWidth', 50);
+
+  assert.ok(group.get('minWidth') === 200, 'should recompute when last inner column changes min width');
+});
